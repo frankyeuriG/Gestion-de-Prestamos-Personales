338c2fc6fc910c0524b054c60c9822c726bc86dc
frankyeuriG <112724688+frankyeuriG@users.noreply.github.com>
2022-09-20 11:53:03 -0400

    Agregando Consulta de personas y poniendo iconos a los botones

--- Models/Persona.cs
+++ Models/Persona.cs
@@ -10,20 +10,20 @@ namespace GestionPrestamos2022.Models
         public int PersonaId {get; set;}
 
        [Required(ErrorMessage ="Es obligatorio introducir el nombre")]
-        public string Nombres { get; set; }
+        public string? Nombres { get; set; }
 
         [Required(ErrorMessage ="Es obligatorio el Correo")]
-        public string Email { get; set; }
+        public string? Email { get; set; }
         
          [Required(ErrorMessage ="Es obligatorio la Direccion")]
-        public string Direccion { get; set; }
+        public string? Direccion { get; set; }
 
         [Required(ErrorMessage = "Complete este Campo")]
         [DataType(DataType.Date)]
         [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd-MM-yyyy}")]
         public DateTime? FechaNacimiento { get; set; }
 
-        public string Telefono {get; set;}
+        public string? Telefono {get; set;}
 
         public int OcupacionId{get; set;}
 
